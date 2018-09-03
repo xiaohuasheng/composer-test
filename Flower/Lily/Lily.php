13a1faecba241dd80c16c7923c58e79bdd59c92d
xhs <775079852@qq.com>
2018-09-03 21:14:39 +0800

    version 1

---
+++ Flower/Lily/Lily.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: xhs
+ * Date: 2018/9/3
+ * Time: 20:56
+ */
+
+namespace Flower\Lily;
+
+
+class Lily
+{
+    public function desc()
+    {
+        echo "this is lily flower";
+    }
+
+}
---
+++ Flower/Rose/Rose.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: xhs
+ * Date: 2018/9/3
+ * Time: 20:57
+ */
+
+namespace Flower\Rose;
+
+
+class Rose
+{
+    public function desc()
+    {
+        echo 'this is rose flower';
+    }
+}
